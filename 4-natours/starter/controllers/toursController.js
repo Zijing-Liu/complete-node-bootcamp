a4f5d37c2b8902f335848591f1d5e2f04cd166a2
Celine Liu <mangolzj@gmail.com>
2023-08-22 16:20:02 -0700

    aggregation pipelines

--- 4-natours/starter/controllers/toursController.js
+++ 4-natours/starter/controllers/toursController.js
@@ -114,3 +114,94 @@ exports.deleteTour = async (req, res) => {
     });
   }
 };
+
+// mongoDB aggregation pipeline
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      // each element in the array will be one of the stages, as objects
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: { $toUpper: '$difficulty' }, // not separate the tours by group, later we could group the tours by difficulty
+          numTours: { $sum: 1 }, // for each document going through this pipeline, 1 will be added to numTours
+          numRatings: { $sum: '$ratingsQuantity' }, // sum of all ratings
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+      {
+        // use the fields of the aggregated results
+        $sort: { avgPrice: 1 },
+      },
+      // {
+      //   // repeat stages, excluding the easy
+      //   $match: { _id: { $ne: 'EASY' } },
+      // },
+    ]);
+    res.status(200).json({
+      status: 'success',
+      results: stats.length,
+      data: {
+        stats,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
+};
+
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1;
+    // Deconstructs an array field from the input documents to output a document for each element.
+    // Each output document is the input document with the value of the array field replaced by the element.
+    const plan = await Tour.aggregate([
+      { $unwind: '$startDates' },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' }, // extract the month from the startDates fields
+          numTourStarts: { $sum: 1 },
+          tours: { $push: '$name' },
+        },
+      },
+      // {
+      //   $addFields: { month: '$_id' }, // name of the fields : value; equivalent to projection
+      // },
+      {
+        $project: {
+          _id: 0, //give each of the fields a 0, meaning not showing the _id
+        },
+      },
+      {
+        $sort: { numTourStarts: -1 }, // sort in descending order
+      },
+      {
+        $limit: 6,
+      },
+    ]);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        plan,
+      },
+    });
+  } catch (err) {
+    // some comments
+  }
+};
